48722ecb3d3c94c3a6d9063c839d00ef5ae71b0a
Kadriye HARMANCI <kadriyeh26@gmail.com>
2026-03-05 10:43:38 +0300

    InClass1

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace InClass1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toplaButton = new System.Windows.Forms.Button();
+            this.cıkarButton = new System.Windows.Forms.Button();
+            this.carpButton = new System.Windows.Forms.Button();
+            this.bolButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Mono", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(46, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 63);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Cascadia Mono", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(283, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 63);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // toplaButton
+            // 
+            this.toplaButton.Font = new System.Drawing.Font("Cascadia Mono", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toplaButton.Location = new System.Drawing.Point(46, 260);
+            this.toplaButton.Name = "toplaButton";
+            this.toplaButton.Size = new System.Drawing.Size(138, 67);
+            this.toplaButton.TabIndex = 2;
+            this.toplaButton.Text = "Topla";
+            this.toplaButton.UseVisualStyleBackColor = true;
+            this.toplaButton.Click += new System.EventHandler(this.toplaButton_Click);
+            this.toplaButton.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+            this.toplaButton.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+            // 
+            // cıkarButton
+            // 
+            this.cıkarButton.Font = new System.Drawing.Font("Cascadia Mono", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cıkarButton.Location = new System.Drawing.Point(217, 260);
+            this.cıkarButton.Name = "cıkarButton";
+            this.cıkarButton.Size = new System.Drawing.Size(138, 67);
+            this.cıkarButton.TabIndex = 3;
+            this.cıkarButton.Text = "Çıkar";
+            this.cıkarButton.UseVisualStyleBackColor = true;
+            this.cıkarButton.Click += new System.EventHandler(this.cıkarButton_Click);
+            this.cıkarButton.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+            this.cıkarButton.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+            // 
+            // carpButton
+            // 
+            this.carpButton.Font = new System.Drawing.Font("Cascadia Mono", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carpButton.Location = new System.Drawing.Point(388, 260);
+            this.carpButton.Name = "carpButton";
+            this.carpButton.Size = new System.Drawing.Size(138, 67);
+            this.carpButton.TabIndex = 4;
+            this.carpButton.Text = "Çarp";
+            this.carpButton.UseVisualStyleBackColor = true;
+            this.carpButton.Click += new System.EventHandler(this.carpButton_Click);
+            this.carpButton.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+            this.carpButton.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+            // 
+            // bolButton
+            // 
+            this.bolButton.Font = new System.Drawing.Font("Cascadia Mono", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bolButton.Location = new System.Drawing.Point(559, 260);
+            this.bolButton.Name = "bolButton";
+            this.bolButton.Size = new System.Drawing.Size(138, 67);
+            this.bolButton.TabIndex = 5;
+            this.bolButton.Text = "Böl";
+            this.bolButton.UseVisualStyleBackColor = true;
+            this.bolButton.Click += new System.EventHandler(this.bolButton_Click);
+            this.bolButton.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+            this.bolButton.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sayı 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(279, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sayı 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(546, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sonuç";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.resultLabel.Font = new System.Drawing.Font("Cascadia Mono", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(550, 65);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(185, 63);
+            this.resultLabel.TabIndex = 8;
+            this.resultLabel.Text = "";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(492, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 46);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "=";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(223, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 46);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "?";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Font = new System.Drawing.Font("Cascadia Mono", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorLabel.Location = new System.Drawing.Point(39, 166);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 37);
+            this.errorLabel.TabIndex = 12;
+            this.errorLabel.Click += new System.EventHandler(this.errorLabel_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(850, 25);
+            this.toolStrip1.TabIndex = 13;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cascadia Mono", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(563, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(241, 37);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Efe Arda YAVAŞ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cascadia Mono", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(563, 414);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(273, 37);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Kadriye HARMANCI";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 479);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bolButton);
+            this.Controls.Add(this.carpButton);
+            this.Controls.Add(this.cıkarButton);
+            this.Controls.Add(this.toplaButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button toplaButton;
+        private System.Windows.Forms.Button cıkarButton;
+        private System.Windows.Forms.Button carpButton;
+        private System.Windows.Forms.Button bolButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace InClass1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            errorLabel.Text = string.Empty;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            errorLabel.Text = string.Empty;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            errorLabel.Text = string.Empty;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            errorLabel.Text = string.Empty; 
+        }
+
+        private bool TryParseInput(string input, out double value)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                value = 0;
+                return false;
+            }
+
+            var normalized = input.Replace(',', '.');
+            return double.TryParse(normalized, NumberStyles.Any, CultureInfo.InvariantCulture, out value);
+        }
+
+        private void cıkarButton_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            if (!TryParseInput(textBox1.Text, out a))
+            {
+                errorLabel.Text = "Sayı 1 geçerli değil.";
+                return;
+            }
+
+            if (!TryParseInput(textBox2.Text, out b))
+            {
+                errorLabel.Text = "Sayı 2 geçerli değil.";
+                return;
+            }
+
+            label5.Text = "-";
+            var result = a - b;
+            resultLabel.Text = result.ToString();
+            resultLabel.BackColor = result > 0 ? Color.SkyBlue: Color.Pink;
+            errorLabel.Text = string.Empty;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void errorLabel_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void toplaButton_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            if (!TryParseInput(textBox1.Text, out a))
+            {
+                errorLabel.Text = "Sayı 1 geçerli değil.";
+                return;
+            }
+
+            if (!TryParseInput(textBox2.Text, out b))
+            {
+                errorLabel.Text = "Sayı 2 geçerli değil.";
+                return;
+            }
+
+            label5.Text = "+";
+            var result = a + b;
+            resultLabel.Text = result.ToString();
+            resultLabel.BackColor = result > 0 ? Color.SkyBlue: Color.Pink;
+            errorLabel.Text = string.Empty;
+        }
+
+        private void carpButton_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            if (!TryParseInput(textBox1.Text, out a))
+            {
+                errorLabel.Text = "Sayı 1 geçerli değil.";
+                return;
+            }
+
+            if (!TryParseInput(textBox2.Text, out b))
+            {
+                errorLabel.Text = "Sayı 2 geçerli değil.";
+                return;
+            }
+
+            label5.Text = "*";
+            var result = a * b;
+            resultLabel.Text = result.ToString();
+            resultLabel.BackColor = result > 0 ? Color.SkyBlue : Color.Pink;
+            errorLabel.Text = string.Empty;
+        }
+
+        private void bolButton_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            if (!TryParseInput(textBox1.Text, out a))
+            {
+                errorLabel.Text = "Sayı 1 geçerli değil.";
+                return;
+            }
+
+            if (!TryParseInput(textBox2.Text, out b))
+            {
+                errorLabel.Text = "Sayı 2 geçerli değil.";
+                return;
+            }
+
+            if (b == 0)
+            {
+                errorLabel.Text = "Sayı 2 sıfır olamaz.";
+                return;
+            }
+
+            label5.Text = "/";
+            var result = a / b;
+            resultLabel.Text = result.ToString();
+            resultLabel.BackColor = result > 0 ? Color.SkyBlue : Color.Pink;
+            errorLabel.Text = string.Empty;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+                btn.ForeColor = Color.Red;
+        }
+
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+                btn.ForeColor = SystemColors.ControlText;
+        }
+    }
+}
