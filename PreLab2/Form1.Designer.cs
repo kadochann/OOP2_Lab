0b0c0c711be6973276891f326af641c1fb9f3b7b
kadochann <147480488+kadochann@users.noreply.github.com>
2026-03-12 00:45:08 +0300

    prelab2 commit

---
+++ PreLab2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace PreLab1
+{
+    // Form1.Designer.cs içindeki kritik kısımlar
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(600, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prelab\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(30, 110);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 180);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(230, 110);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(140, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Calculate Average";
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(230, 145);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(18, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.AutoSize = true;
+            this.labelAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.labelAverage.Location = new System.Drawing.Point(230, 190);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(108, 20);
+            this.labelAverage.TabIndex = 0;
+            this.labelAverage.Text = "AVERAGE: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate and Calculate";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(788, 331);
+            this.Controls.Add(this.labelAverage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Prelab";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PreLab2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PreLab1
+{
+    public partial class Form1 : Form
+    {
+        int[] dizi; // Checkpoint 2: Array tanımı
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Sayı kontrolü için event bağlama
+            this.textBox1.KeyPress += textBox1_KeyPress;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Checkpoint 3: Buton metni güncelleme
+            button1.Text = checkBox1.Checked ? "Generate and Calculate" : "Generate";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int boyut) && boyut > 0)
+            {
+                // Diziyi yeniden boyutlandır ve listeyi temizle
+                dizi = new int[boyut];
+                listBox1.Items.Clear();
+                double toplam = 0;
+
+                for (int i = 0; i < boyut; i++)
+                {
+                    dizi[i] = i + 1;
+                    listBox1.Items.Add(dizi[i]);
+                    toplam += dizi[i];
+                }
+
+                // Ortalama hesabı ve label güncelleme
+                if (checkBox1.Checked)
+                {
+                    double ortalama = toplam / boyut;
+                    labelAverage.Text = "AVERAGE: " + ortalama.ToString();
+                }
+                else
+                {
+                    labelAverage.Text = "AVERAGE: -";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir sayı girin!");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Checkpoint 1: Sadece rakam girişi
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Kullanılmayan metodlar
+        private void label1_Click(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
